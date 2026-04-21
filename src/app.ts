699100433d87c775796293da938e7bbde4687f31
Jeferson Silva <jefersonsilva.contato@gmail.com>
2026-04-20 22:51:07 -0300

    Add error handling for user registration; implement Zod validation error responses and create UserAlreadyExistsError class for better error management

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,23 @@
 import fastfy from 'fastify'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastfy()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+  // Here we should log to an external tool like Sentry, Datadog, LogRocket, etc.
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
