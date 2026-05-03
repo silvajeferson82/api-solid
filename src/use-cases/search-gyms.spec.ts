f00dadabbf054f3909727882738c4261ad9db4f2
Jeferson Silva <jefersonsilva.contato@gmail.com>
2026-05-03 17:35:51 -0300

    feat: implement gym search use case with pagination
    Add SearchGymsUseCase to enable searching gyms by query string with paginated results. Implement searchMany method in gyms repository interface and in-memory implementation to filter gyms by title and return 20 results per page.

---
+++ src/use-cases/search-gyms.spec.ts
@@ -0,0 +1,60 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { randomUUID } from 'node:crypto'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { SearchGymsUseCase } from './search-gyms'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: SearchGymsUseCase
+
+describe('Search Gyms Use Case', () => {
+  const userId = randomUUID()
+  const gym_title_1 = 'Javascript Gym'
+  const gym_title_2 = 'Boot Strap Gym'
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new SearchGymsUseCase(gymsRepository)
+  })
+
+  it('should be able to search for gyms', async () => {
+    await gymsRepository.create({
+      title: gym_title_1,
+      description: null,
+      phone: null,
+      latitude: 0,
+      longitude: 0,
+    })
+
+    await gymsRepository.create({
+      title: gym_title_2,
+      description: null,
+      phone: null,
+      latitude: 0,
+      longitude: 0,
+    })
+
+    const { gyms } = await sut.execute({ query: gym_title_1, page: 1 })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([expect.objectContaining({ title: gym_title_1 })])
+  })
+
+  it('should be able to fetch paginated gyms search', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await gymsRepository.create({
+        title: `Javascript Gym ${i}`,
+        description: null,
+        phone: null,
+        latitude: 0,
+        longitude: 0,
+      })
+    }
+
+    const { gyms } = await sut.execute({ query: 'Javascript', page: 2 })
+
+    expect(gyms).toHaveLength(2)
+    expect(gyms).toEqual([
+      expect.objectContaining({ title: 'Javascript Gym 21' }),
+      expect.objectContaining({ title: 'Javascript Gym 22' }),
+    ])
+  })
+})
